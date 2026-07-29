3f279e6328a6db909395808b7e4cef6630d3dced
dilyaniliev7 <dilyaniliev7@abv.bg>
2026-07-29 23:18:27 +0300

    list, update & delete

--- backend/blogapp/urls.py
+++ backend/blogapp/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path("register_user/", views.register_user, name="register_user"),
-    path("create_blog/", views.create_blog, name="create_blog")
+    path("create_blog/", views.create_blog, name="create_blog"),
+    path("blog_list", views.blog_list, name="blog_list"),
+    path("update_blog/<int:pk>/", views.update_blog, name="update_blog"),
+    path("delete_blog/<int:pk>/", views.delete_blog, name="delete_blog")
 ]
--- backend/blogapp/views.py
+++ backend/blogapp/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
+from .models import Blog
 
 @api_view(["POST"])
 def register_user(request):
@@ -23,3 +24,35 @@ def create_blog(request):
         serializer.save(author=user)
         return Response(serializer.data)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET"])
+def blog_list(request):
+    blogs = Blog.objects.all()
+    serializer = BlogSerializer(blogs, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def update_blog(request, pk):
+    user = request.user
+    blog = Blog.objects.get(id=pk)
+    if blog.author != user:
+        return Response({"error": "You are not the author of this blog"}, status=status.HTTP_403_FORBIDDEN)
+    serializer = BlogSerializer(blog, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def delete_blog(request, pk):
+    blog = Blog.objects.get(id=pk)
+    user = request.user
+    if blog.author != user:
+        return Response({"error": "You are not the author of this blog"}, status=status.HTTP_403_FORBIDDEN)
+    blog.delete()
+    return Response({"message": "Blog deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
