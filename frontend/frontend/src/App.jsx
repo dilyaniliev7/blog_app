4bcd74145e82f84599405c1519a60f57f9c75b3d
dilyaniliev7 <dilyaniliev7@abv.bg>
2026-07-30 23:57:20 +0300

    tailwindcss & react setup

---
+++ frontend/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+const App = () => {
+    return  (
+        <div>
+            Hellow World
+            <Button className="bg-blue-800">Click Me</Button>
+        </div>
+    )
+}
+
+export default App
