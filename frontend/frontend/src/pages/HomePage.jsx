8e791a027991dd7ecd6077f6b56182687a0ddb3d
dilyaniliev7 <dilyaniliev7@abv.bg>
2026-08-05 22:14:44 +0300

    data fetching with React Query

--- frontend/frontend/src/pages/HomePage.jsx
+++ frontend/frontend/src/pages/HomePage.jsx
@@ -1,11 +1,20 @@
+import { getBlogs } from "@/services/apiBlog"
 import Header from "@/ui_components/Header"
 import BlogContainer from "@/ui_components/BlogContainer"
+import { useQuery } from "@tanstack/react-query"
 
 const HomePage = () => {
+
+    const {isPending, isError, error, data:blogs} = useQuery({
+        queryKey: ['blogs'],
+        queryFn: getBlogs
+    })
+
+
     return (
         <>
             <Header />
-            <BlogContainer />
+            <BlogContainer isPending={isPending} blogs={blogs}/>
         </>
     )
 }
--- frontend/frontend/src/ui_components/BlogContainer.jsx
+++ frontend/frontend/src/ui_components/BlogContainer.jsx
@@ -1,6 +1,12 @@
 import BlogCard from "./BlogCard"
+import Spinner from "./Spinner"
 
-const BlogContainer = () => {
+const BlogContainer = ({isPending, blogs}) => {
+
+    if(isPending){
+        return <Spinner />
+    }
+
     return (
         <section className="padding-x py-6 max-container">
             <h2 className="font-semibold  text-xl mb-6 dark:text-white text-center">
@@ -8,13 +14,8 @@ const BlogContainer = () => {
             </h2>
 
             <div className="flex items-center gap-6 justify-center flex-wrap">
-                <BlogCard />
-                <BlogCard />
-                <BlogCard />
+                {blogs.map((blog) => <BlogCard key={blog.id} blog={blog} />)}
 
-                <BlogCard />
-                <BlogCard />
-                <BlogCard />
             </div>
         </section>
     )
